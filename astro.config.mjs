5dd045c905dbfb198f2969c524a5e5754df37bee
IC3P3 <n.eifler@freenet.de>
2026-02-26 20:34:23 +0100

    added basic packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import svelte from '@astrojs/svelte';
+
+import sitemap from '@astrojs/sitemap';
+
+import robotsTxt from 'astro-robots-txt';
+
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [svelte(), sitemap(), robotsTxt()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
