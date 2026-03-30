5495c2f427235e21c14b07c8e6e60bb01349ab1b
IC3P3 <n.eifler@freenet.de>
2026-03-30 21:40:39 +0200

    Added some qc tooling and removed unneeded files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,20 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import svelte from "@astrojs/svelte";
-
 import sitemap from "@astrojs/sitemap";
-
 
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://neifler.de",
-  i18n: {
-    defaultLocale: "de",
-    locales: ["de"],
-  },
-  integrations: [svelte(), sitemap()],
+    site: "https://neifler.de",
+    i18n: {
+        defaultLocale: "de",
+        locales: ["de"],
+    },
+    integrations: [sitemap()],
 
-  vite: {
-    plugins: [tailwindcss()],
-  },
+    vite: {
+        plugins: [tailwindcss()],
+    },
 });
