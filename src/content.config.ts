7acece438a70d28244d20b9dbd874c3dc4fec769
IC3P3 <n.eifler@freenet.de>
2026-05-15 22:09:16 +0200

    Reversed order of projects

--- src/content.config.ts
+++ src/content.config.ts
@@ -14,7 +14,6 @@ const projects = defineCollection({
             live: z.string().url().optional(),
         }),
         type: z.enum(["web", "app"]).default("web"),
-        order: z.number(),
     }),
 });
 
